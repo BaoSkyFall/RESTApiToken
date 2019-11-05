63cd0609ed726ec9e71b66cec920e150c260d4ce
BaoSkyFall <baoit128@gmail.com>
2019-11-05 11:49:03 +0700

    Update Cors

--- app.js
+++ app.js
@@ -3,29 +3,35 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
-
 var app = express();
+var server = app.listen(3000);
+var io = module.exports.io=require('socket.io').listen(server);
+// io.on("connection", () => {
+//   console.log("Connected!");
+// });
+const SocketManager = require('./socket/socketManager')
 
+io.on('connection', SocketManager)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+
 
 // error handler
 app.use(function (err, req, res, next) {
